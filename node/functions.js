bea9ac51a8ebb39d9462f5ea6222f059015c7587
rajvishah25 <shah.rajvi54@gmail.com>
2019-02-06 20:28:01 -0800

    function crazyness

--- node/functions.js
+++ node/functions.js
@@ -3,17 +3,23 @@ function hi() {
   console.log('Say Hello');
 }
 
-hi();
+hi(); //dont use normal funcion because it lets you call functions before they were declared
 
 // arrow functions
 const arrow = (test) => {
   console.log(test);
 }
 
-arrow('hi')
+arrow('hi');
 
 // for practice
 
 // make an arrow function with no block body
-
+const myArrow = (y) => y + 1; //returns a value
+console.log(myArrow(8));
 // make a function that returns a function
+const functionFactory = () => () => console.log("This is weird :P");
+const a = functionFactory;
+a();
+const anotherFunction = functionFactory;
+anotherFunction()();
