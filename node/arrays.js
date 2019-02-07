7465eaf1270fb07dee18e8c9915e510b1e842bb2
rajvishah25 <shah.rajvi54@gmail.com>
2019-02-06 20:37:49 -0800

    arrays

--- node/arrays.js
+++ node/arrays.js
@@ -15,15 +15,25 @@ array[1] = 'hello';
 // for practice
 
 // use forEach
-
+const arrayTest = [9, 3, 4];
+arrayTest[4] = 9;
+console.log(arrayTest);
+arrayTest.forEach((n) => {
+    console.log(n);
+});
 // use map
-
+const newArray = arrayTest.map(i => i + 1);
+console.log(newArray);
 // use pop
-
+newArray.pop();
+console.log(newArray);
 // use push
-
+newArray.push(6);
+console.log(newArray);
 // use shift
-
+newArray.shift();
+console.log(newArray);
 // use unshift
-
+newArray.unshift(5);
+console.log(newArray);
 // use filter
